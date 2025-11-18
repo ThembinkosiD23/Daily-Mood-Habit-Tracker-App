b3476f508944a37aae38e5ab64f48c5f4c4a95fa
ThembinkosiD23 <dladlathembinkosi75@gmail.com>
2025-11-18 14:15:33 +0200

    Feat: added the DisplayMenu

--- Daily Mood & Habit Tracker App/Program.cs
+++ Daily Mood & Habit Tracker App/Program.cs
@@ -1,7 +1,8 @@
-﻿internal class Program
+﻿using System.ComponentModel.DataAnnotations;
+
+internal class Program
 {
-
-
+    List<DailyLog> DailyLogs = new List<DailyLog>();
     public void AddDailyLog()
     {
 
@@ -18,8 +19,30 @@
     {
 
     }
+    public void Option(string option)
+    {
+
+    }
+    public void DisplayMenu()
+    {
+        Console.WriteLine("Please enter your choose: ");
+        Console.WriteLine("1. Add the daily log. ");
+        Console.WriteLine("2. View Past logs. ");
+        Console.WriteLine("3. Analyse My Week ");
+        Console.WriteLine("4. Exits");
+        string option = Console.ReadLine();
+
+    }
     private static void Main(string[] args)
     {
         Console.WriteLine("---------------WELCOME TO--------------- \n ------Daily Mood & Habit Tracker------");
     }
+}
+
+class DailyLog
+{
+    public string mood;
+    public string exercise;
+    public string meditate;
+    public string hoursOfSleep;
 }
