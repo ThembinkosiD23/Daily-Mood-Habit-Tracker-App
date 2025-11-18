c33bbe5b814549baa41aa706312557da084bfe0a
ThembinkosiD23 <dladlathembinkosi75@gmail.com>
2025-11-18 13:52:24 +0200

    Project setup

--- Daily Mood & Habit Tracker App/Program.cs
+++ Daily Mood & Habit Tracker App/Program.cs
@@ -1,5 +1,23 @@
 ﻿internal class Program
 {
+
+
+    public void AddDailyLog()
+    {
+
+    }
+     public void ViewPastLogs()
+    {
+
+    }
+    public void AnalaysWeek()
+    {
+
+    }
+    public void Exits()
+    {
+
+    }
     private static void Main(string[] args)
     {
         Console.WriteLine("---------------WELCOME TO--------------- \n ------Daily Mood & Habit Tracker------");
